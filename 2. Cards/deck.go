995bae76fe5d5cf3bc6dbd0e753a2011f31b9263
Rahul Bajaj <rahulrb0509@gmail.com>
2021-06-21 22:49:34 +0530

    add deck of cards

---
+++ 2. Cards/deck.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+// Create a new type of 'deck'
+// which is a slice of strings
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Clubs", "Diamonds"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	// use _ as a placeholder for a variable that does not need to be used
+	// In this case _ is used as a placeholder for the index in the slice
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+// (d deck) indicates the receiver. The print method will receive a value
+// of type deck and it can be used in the function as 'd'
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
